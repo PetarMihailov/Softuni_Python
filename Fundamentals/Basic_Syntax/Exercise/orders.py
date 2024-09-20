e392de403901b98e0ae16ab1166a183e0d44f2e5
pmihailov <petar_mihailov@gbg.bg>
2024-09-21 00:18:32 +0300

    Update orders.py
    change price_per_capsule range

--- Fundamentals/Basic_Syntax/Exercise/orders.py
+++ Fundamentals/Basic_Syntax/Exercise/orders.py
@@ -6,7 +6,7 @@ for _ in range(orders):
     price_per_capsule = float(input())
     days = int(input())
     capsules_count = int(input())
-    if 0 < price_per_capsule <= 100 and 1 <= days <= 31 and 1 <= capsules_count <= 2000:
+    if 0.01 <= price_per_capsule <= 100 and 1 <= days <= 31 and 1 <= capsules_count <= 2000:
         total_per_order = days * capsules_count * price_per_capsule
         total_price += total_per_order
         print(f"The price for the coffee is: ${total_per_order:.2f}")
