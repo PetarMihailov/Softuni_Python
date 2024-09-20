a9c033980c6537a75ca81e819f42bcd46bc2b273
pmihailov <petar_mihailov@gbg.bg>
2024-09-20 21:27:42 +0300

    Create chat_codes.py

---
+++ Fundamentals/Basic_Syntax/Exercise/chat_codes.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    num = int(input())
+    if num == 88:
+        print("Hello")
+    elif num == 86:
+        print("How are you?")
+    elif num < 88:
+        print("GREAT!")
+    else:
+        print("Bye.")
