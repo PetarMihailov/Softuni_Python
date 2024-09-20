419ea1966820dc435c8d1b77f9aa930f6b646aaf
pmihailov <petar_mihailov@gbg.bg>
2024-09-20 19:33:09 +0300

    Update and rename patterns to patterns.py
    added second solution

--- Fundamentals/Basic_Syntax/Lab/patterns.py
+++ Fundamentals/Basic_Syntax/Lab/patterns.py
@@ -9,3 +9,13 @@ for i in range(n-1, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
+
+# second solution
+
+n = int(input())
+
+for i in range(1, n + 1):
+    print('*' * i)
+
+for i in range(n - 1, 0, -1):
+    print('*' * i)
