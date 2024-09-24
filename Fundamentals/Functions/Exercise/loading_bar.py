dd1170b73c962dbc175ec783cc29e56824a43497
pmihailov <petar_mihailov@gbg.bg>
2024-09-24 19:51:55 +0300

    Create loading_bar.py

---
+++ Fundamentals/Functions/Exercise/loading_bar.py
@@ -0,0 +1,13 @@
+def loading_bar(n):
+    bar = [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    n //= 10
+
+    for index in range(n):
+        bar[index] = "%"
+    if n == 10:
+        return f"100% Complete!\n[{''.join(bar)}]"
+    return f'{n*10}% [{"".join(bar)}]\nStill loading...'
+
+percentage = int(input())
+
+print(loading_bar(percentage))
