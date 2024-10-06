7b3810194bbfe1d1a8510aa5e86ff7e9e4295ecb
pmihailov <petar_mihailov@gbg.bg>
2024-10-06 07:14:29 +0300

    Update cups_and_bottles.py
    solution with while True

--- Advanced/Lists_as_Stacks_and_Queues/Exercise/cups_and_bottles.py
+++ Advanced/Lists_as_Stacks_and_Queues/Exercise/cups_and_bottles.py
@@ -18,3 +18,30 @@ else:
     print(f"Bottles:", *bottles)
 
 print(f"Wasted litters of water: {waste}")
+
+
+# with while True 
+
+from collections import deque
+
+cups = deque(list(map(int, input().split())))
+bottles = list(map(int, input().split()))
+
+waste = 0
+
+while True:
+    if not bottles or not cups:
+        break
+
+    if bottles[-1] >= cups[0]:
+        waste += bottles.pop() - cups.popleft()
+    elif cups[0] >= 0:
+        cups[0] -= bottles.pop()
+
+
+if cups:
+    print(f"Cups:", *cups)
+else:
+    print(f"Bottles:", *bottles)
+
+print(f"Wasted litters of water: {waste}")
