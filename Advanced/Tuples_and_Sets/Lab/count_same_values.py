39b00e081d6f9326bdff0b18c6e6d23f2166ac4c
pmihailov <petar_mihailov@gbg.bg>
2024-10-08 20:06:25 +0300

    Update count_same_values.py

--- Advanced/Tuples_and_Sets/Lab/count_same_values.py
+++ Advanced/Tuples_and_Sets/Lab/count_same_values.py
@@ -3,3 +3,18 @@ numbers = list(map(float, input().split()))
 counts = {key: numbers.count(key) for key in numbers}
 
 print('\n'.join([f"{data[0]} - {data[1]} times" for data in counts.items()]))
+
+
+# solution 2
+
+numbers = (float(el) for el in input().split())
+
+occ = {}
+
+for el in numbers:
+    if el not in occ:
+        occ[el] = 0
+    occ[el] += 1
+
+for el in occ:
+    print(f"{el} - {occ[el]} times")
