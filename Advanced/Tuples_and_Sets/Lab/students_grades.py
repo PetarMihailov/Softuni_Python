cf56add6f89f10e1559a239aa67955eefd56563a
pmihailov <petar_mihailov@gbg.bg>
2024-10-09 04:44:35 +0300

    Update students_grades.py

--- Advanced/Tuples_and_Sets/Lab/students_grades.py
+++ Advanced/Tuples_and_Sets/Lab/students_grades.py
@@ -12,3 +12,21 @@ for _ in range(n):
 
 for key, value in students.items():
     print(f"{key} -> {' '.join([f'{el:.2f}' for el in value])} (avg: {sum(value)/len(value):.2f})")
+
+# soltion with mean
+
+from statistics import mean
+
+students = int(input())
+
+grades = {}
+
+for _ in range(students):
+    name, grade = input().split()
+    grade = float(grade)
+    if name not in grades:
+        grades[name] = []
+    grades[name].append(grade)
+
+for data in grades.items():
+    print(f"{data[0]} -> {' '.join([f'{el:.2f}' for el in data[1]])} (avg: {mean(data[1]):.2f})")
