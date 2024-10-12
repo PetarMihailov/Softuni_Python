27a389be3058e8983c1de3db1bc98524c78974e6
pmihailov <petar_mihailov@gbg.bg>
2024-10-12 14:31:35 +0300

    Update reverse_numbers.py

--- Advanced/Lists_as_Stacks_and_Queues/Exercise/reverse_numbers.py
+++ Advanced/Lists_as_Stacks_and_Queues/Exercise/reverse_numbers.py
@@ -2,3 +2,9 @@ stack = [int(el) for el in input().split()]
 
 while stack:
     print(stack.pop(), end=" ")
+
+# second solution
+
+numbers = list(map(int, input().split()))
+
+[print(numbers[index], end=" ") for index in range(len(numbers)-1, -1, -1)]
