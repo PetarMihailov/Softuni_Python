3921656183994b072c155f5881deed3a841db3dc
pmihailov <petar_mihailov@gbg.bg>
2024-10-13 16:09:09 +0300

    Update numbers.py

--- Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/numbers.py
+++ Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/numbers.py
@@ -23,4 +23,33 @@ print(*sorted(first_sequence), sep=", ")
 print(", ".join([str(el) for el in sorted(list(second_sequence))]))
 
 
+# second solution
+
+first_sequence = set(list(map(int, input().split())))
+second_sequence = set(list(map(int, input().split())))
+
+line = int(input())
+
+for _ in range(line):
+    data = input().split()
+
+    if data[0] == "Add":
+        if data[1] == "First":
+            [first_sequence.add(int(el)) for el in data[2:]]
+        else:
+            [second_sequence.add(int(el)) for el in data[2:]]
+    elif data[0] == "Remove":
+        if data[1] == "First":
+            for el in data[2:]:
+                first_sequence.discard(int(el))
+        else:
+            for el in data[2:]:
+                second_sequence.discard(int(el))
+    else:
+        print(first_sequence.issubset(second_sequence) or second_sequence.issubset(first_sequence))
+
+print(*sorted(first_sequence), sep=", ")
+print(", ".join([str(el) for el in sorted(list(second_sequence))]))
+
+
 
