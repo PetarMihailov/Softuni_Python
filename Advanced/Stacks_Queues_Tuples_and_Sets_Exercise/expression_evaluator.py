9b33e7d9b77ac9189841ab9c20c8af02b3c36efb
pmihailov <petar_mihailov@gbg.bg>
2024-10-13 19:38:44 +0300

    Create expression_evaluator.py

---
+++ Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/expression_evaluator.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+elements = input().split()
+numbers = deque()
+
+
+def operation(operator, a, b):
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return a // b
+
+
+for el in elements:
+    if el in "+-*/":
+        while len(numbers) > 1:
+            first = numbers.popleft()
+            second = numbers.popleft()
+
+            result = operation(el, first, second)
+            numbers.appendleft(result)
+
+    else:
+        numbers.append(int(el))
+
+print(numbers.popleft())
