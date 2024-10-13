b387405ce15b439a12f6849c5d5b0a5b53397071
pmihailov <petar_mihailov@gbg.bg>
2024-10-13 04:42:17 +0300

    Update phonebook.py

--- Fundamentals/Dictionaries/Exercise/phonebook.py
+++ Fundamentals/Dictionaries/Exercise/phonebook.py
@@ -16,3 +16,29 @@ for _ in range(line):
         print(f"{name} -> {phonebook[name]}")
     else:
         print(f"Contact {name} does not exist.")
+
+# second solution
+
+
+
+def fill_phonebook():
+    data = input()
+    phonebook = {}
+    while not data.isdigit():
+        name, number = data.split("-")
+        phonebook[name] = number
+        data = input()
+    return phonebook, data
+
+def search_contact(contact_name, phonebook):
+    if phonebook.get(contact_name):
+        print(f"{contact_name} -> {phonebook[contact_name]}")
+    else:
+        print(f"Contact {contact_name} does not exist.")
+
+
+
+phone_book, line = fill_phonebook()
+
+for _ in range(int(line)):
+    search_contact(input(), phone_book)
