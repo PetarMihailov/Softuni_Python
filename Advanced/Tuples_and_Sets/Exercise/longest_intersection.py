fc805cabab598bb2209d69098cb67f5c44496ff0
pmihailov <petar_mihailov@gbg.bg>
2024-10-13 05:16:49 +0300

    Update longest_intersection.py

--- Advanced/Tuples_and_Sets/Exercise/longest_intersection.py
+++ Advanced/Tuples_and_Sets/Exercise/longest_intersection.py
@@ -13,3 +13,21 @@ for _ in range(n):
 sort = sorted(intersections, key=lambda x: -len(x))[0]
 
 print(f"Longest intersection is {list(sort)} with length {len(sort)}")
+
+# solution 2
+
+lines = int(input())
+
+longest_intersection = []
+
+for _ in range(lines):
+    first_data, second_data = input().split("-")
+    first_start, first_end = [int(el) for el in first_data.split(",")]
+    second_start, second_end = [int(el) for el in second_data.split(",")]
+
+    intersection = range(max(first_start, second_start), min(first_end, second_end) + 1)
+    longest_intersection.append(intersection)
+
+longest_intersection= sorted(longest_intersection, key=lambda x: -len(x))[0]
+
+print(f"Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}")
