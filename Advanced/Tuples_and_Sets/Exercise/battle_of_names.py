26665aa59f8a460da0bffdded75ce2ab086a882a
pmihailov <petar_mihailov@gbg.bg>
2024-10-13 05:42:36 +0300

    Update battle_of_names.py

--- Advanced/Tuples_and_Sets/Exercise/battle_of_names.py
+++ Advanced/Tuples_and_Sets/Exercise/battle_of_names.py
@@ -22,3 +22,30 @@ else:
     result = list(map(str, odd.symmetric_difference(even)))
     print(", ".join(result))
 
+# solution 2
+
+lines = int(input())
+
+even_numbers = set()
+odd_numbers = set()
+
+for row in range(1, lines+1):
+    current_sum = sum([ord(el) for el in input()]) // row
+
+    if current_sum % 2 == 0:
+        even_numbers.add(current_sum)
+    else:
+        odd_numbers.add(current_sum)
+
+sum_evens = sum(even_numbers)
+sum_odds = sum(odd_numbers)
+
+if sum_evens == sum_odds:
+    str_set = [str(el) for el in odd_numbers.union(even_numbers)]
+    print(", ".join(str_set))
+elif sum_evens < sum_odds:
+    str_set = [str(el) for el in odd_numbers.difference(even_numbers)]
+    print(", ".join(str_set))
+else:
+    str_set = [str(el) for el in odd_numbers.symmetric_difference(even_numbers)]
+    print(", ".join(str_set))
