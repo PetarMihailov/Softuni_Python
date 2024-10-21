c556730c3928686be108013aef571d27b008e529
pmihailov <petar_mihailov@gbg.bg>
2024-10-21 05:11:27 +0300

    Update numbers.py

--- Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/numbers.py
+++ Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/numbers.py
@@ -51,5 +51,27 @@ for _ in range(line):
 print(*sorted(first_sequence), sep=", ")
 print(", ".join([str(el) for el in sorted(list(second_sequence))]))
 
+# third solution
+
+set_1 = set([int(el) for el in input().split()])
+set_2 = set([int(el) for el in input().split()])
+
+lines = int(input())
+
+for _ in range(lines):
+    data = input().split()
+
+    if data[0] == "Add" and data[1] ==  "First":
+        set_1.update(set([int(el) for el in data[2:]]))
+    elif data[0] == "Add" and data[1] == "Second":
+        set_2.update(set([int(el) for el in data[2:]]))
+    elif data[0] == "Remove" and data[1] == "First":
+        set_1 = set_1.difference(set([int(el) for el in data[2:]]))
+    elif data[0] == "Remove" and data[1] == "Second":
+        set_2 = set_2.difference(set([int(el) for el in data[2:]]))
+    elif data[0] == "Check" and data[1] == "Subset":
+        print(set_1.issubset(set_2) or set_2.issubset(set_1))
+
+print(*sorted(set_1), sep=", ")
 
 
