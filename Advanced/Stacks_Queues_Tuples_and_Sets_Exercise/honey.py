e2f8a92ae2634b472ce0145463ccbb868f9872ae
pmihailov <petar_mihailov@gbg.bg>
2024-10-26 05:53:58 +0300

    Create honey.py

---
+++ Advanced/Stacks_Queues_Tuples_and_Sets_Exercise/honey.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+bees = deque([int(el) for el in input().split()])
+nectars = [int(el) for el in input().split()]
+symbols = deque(input().split())
+
+honey = 0
+
+operators = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y
+}
+
+while bees and nectars:
+    bee = bees[0]
+    nectar = nectars.pop()
+
+    if bee > nectar:
+        continue
+
+    bees.popleft()
+    operator = symbols.popleft()
+
+    try:
+        honey += abs(operators[operator](bee, nectar))
+    except:
+        pass
+
+
+
+print(f"Total honey made: {honey}")
+
+if bees:
+    print(f"Bees left: {', '.join(map(str, bees))}")
+if nectars:
+    print(f"Nectar left: {', '.join(map(str, nectars))}")
