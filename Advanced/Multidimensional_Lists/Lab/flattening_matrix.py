dbaf511ab8fe19ea20b412b2ad2646e04fc04e16
pmihailov <petar_mihailov@gbg.bg>
2024-10-31 05:27:38 +0200

    Update flattening_matrix.py

--- Advanced/Multidimensional_Lists/Lab/flattening_matrix.py
+++ Advanced/Multidimensional_Lists/Lab/flattening_matrix.py
@@ -10,3 +10,18 @@ print(matrix)
 
 # 
 
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    nums =  [int(el) for el in input().split(", ")]
+    matrix.append(nums)
+
+result = []
+
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        result.append(matrix[row][col])
+
+print(result)
