c35aaaa1e440807d97c6cbed2a01df60d23dec47
pmihailov <petar_mihailov@gbg.bg>
2024-10-31 05:13:42 +0200

    Update even_matrix.py

--- Advanced/Multidimensional_Lists/Lab/even_matrix.py
+++ Advanced/Multidimensional_Lists/Lab/even_matrix.py
@@ -8,5 +8,10 @@ for _ in range(rows):
 
 print(matrix)
 
-# 
+# second solution
 
+rows = int(input())
+
+matrix = [[int(el) for el in input().split(", ") if int(el) % 2 == 0]for _ in range(rows)]
+
+print(matrix)
