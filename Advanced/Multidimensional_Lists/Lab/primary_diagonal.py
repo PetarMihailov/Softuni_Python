f6d0378eb2d37823bcb59290d6161008f0b18aa6
pmihailov <petar_mihailov@gbg.bg>
2024-11-01 05:39:50 +0200

    Update primary_diagonal.py

--- Advanced/Multidimensional_Lists/Lab/primary_diagonal.py
+++ Advanced/Multidimensional_Lists/Lab/primary_diagonal.py
@@ -12,3 +12,21 @@ for i in range(n):
     diagonal += matrix[i][i]
 
 print(diagonal)
+
+# second solution
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    matrix.append([int(el) for el in input().split()])
+
+result = 0
+
+for row in range(rows):
+    for col in range(rows):
+        if row == col:
+            result += matrix[row][col]
+
+print(result)
