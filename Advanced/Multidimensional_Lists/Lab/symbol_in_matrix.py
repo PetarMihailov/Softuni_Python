1afe9c17f0c00a43a55b43704ccf67b52edcf7e8
pmihailov <petar_mihailov@gbg.bg>
2024-11-01 05:49:56 +0200

    Update symbol_in_matrix.py

--- Advanced/Multidimensional_Lists/Lab/symbol_in_matrix.py
+++ Advanced/Multidimensional_Lists/Lab/symbol_in_matrix.py
@@ -21,3 +21,25 @@ if position:
     print(position)
 else:
     print(f"{searched_char} does not occur in the matrix")
+
+# second solution
+
+n = int(input())
+
+matrix = []
+position = None
+
+for _ in range(n):
+    matrix.append([el for el in list(input())])
+
+symbol = input()
+
+for row in range(n):
+    for col in range(n):
+        if matrix[row][col] == symbol:
+            position = (row, col)
+            break
+    if position:
+        break
+
+print(position if position else f"{symbol} does not occur in the matrix")
