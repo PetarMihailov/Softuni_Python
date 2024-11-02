234242d3fda577f92aaf0d8dec799c3a578558e9
pmihailov <petar_mihailov@gbg.bg>
2024-11-02 19:49:35 +0200

    Update square_with_maximum_sum.py

--- Advanced/Multidimensional_Lists/Lab/square_with_maximum_sum.py
+++ Advanced/Multidimensional_Lists/Lab/square_with_maximum_sum.py
@@ -51,3 +51,31 @@ for row in range(rows-1, 0, -1):
 print(max_sub_matrix[0], max_sub_matrix[1])
 print(max_sub_matrix[2], max_sub_matrix[3])
 print(max_sum)
+
+# third solution 
+
+import sys
+
+rows, cols = [int(el) for el in input().split(", ")]
+
+matrix = []
+
+for _ in range(rows):
+    matrix.append([int(el) for el in input().split(", ")])
+
+max_sub_matrix = None
+max_sum = -sys.maxsize
+
+for row in range(rows-1):
+    for col in range(cols-1):
+        sub_matrix = [matrix[row][col], matrix[row][col+1], matrix[row+1][col], matrix[row+1][col+1]]
+
+        current_sum = sum(sub_matrix)
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            max_sub_matrix = sub_matrix
+
+print(max_sub_matrix[0], max_sub_matrix[1])
+print(max_sub_matrix[2], max_sub_matrix[3])
+print(max_sum)
