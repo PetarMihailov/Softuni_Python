26837241b649d89aac751b2e5a6172c9e86d8d02
pmihailov <petar_mihailov@gbg.bg>
2024-11-03 03:31:15 +0200

    Update diagonal_difference.py

--- Advanced/Multidimensional_Lists/Exercise/diagonal_difference.py
+++ Advanced/Multidimensional_Lists/Exercise/diagonal_difference.py
@@ -17,3 +17,18 @@ for index in range(size):
     secondary_diagonal.append(matrix[index][size - index - 1])
 
 print(abs(sum(primary_diagonal) - sum(secondary_diagonal)))
+
+# second solution
+
+matrix = [[int(el) for el in input().split()] for n in range(int(input()))]
+
+first = 0
+second = 0
+col = len(matrix) - 1
+
+for index in range(len(matrix)):
+    first += matrix[index][index]
+    second += matrix[index][col]
+    col -= 1
+
+print(abs(first-second))
