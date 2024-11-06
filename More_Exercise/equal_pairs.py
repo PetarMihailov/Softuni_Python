b35fd61040f9aa4912b4849d0f567854eb2006c4
pmihailov <petar_mihailov@gbg.bg>
2024-11-06 15:31:47 +0200

    Create equal_pairs.py

---
+++ More_Exercise/equal_pairs.py
@@ -0,0 +1,24 @@
+import sys
+
+n = int(input())
+
+result = []
+max_diff = -sys.maxsize
+
+for _ in range(n):
+    first, second = int(input()), int(input())
+    result.append((first, second))
+
+equal = True
+
+for index in range(n-1):
+    diff = abs(sum(result[index]) - sum(result[index+1]))
+    if diff:
+        equal = False
+    if diff > max_diff:
+        max_diff = diff
+
+if equal:
+    print(f"Yes, value={sum(result[0])}")
+else:
+    print(f"No, maxdiff={max_diff}")
