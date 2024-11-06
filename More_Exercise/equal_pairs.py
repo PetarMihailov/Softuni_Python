702756130fc71b162f787db5194fa184778a72c9
pmihailov <petar_mihailov@gbg.bg>
2024-11-06 15:38:56 +0200

    Update equal_pairs.py

--- More_Exercise/equal_pairs.py
+++ More_Exercise/equal_pairs.py
@@ -22,3 +22,26 @@ if equal:
     print(f"Yes, value={sum(result[0])}")
 else:
     print(f"No, maxdiff={max_diff}")
+
+# second solution
+
+import sys
+
+n = int(input())
+
+result = [(int(input()), int(input())) for _ in range(n)]
+max_diff = -sys.maxsize
+
+equal = True
+
+for index in range(n-1):
+    diff = abs(sum(result[index]) - sum(result[index+1]))
+    if diff:
+        equal = False
+    if diff > max_diff:
+        max_diff = diff
+
+if equal:
+    print(f"Yes, value={sum(result[0])}")
+else:
+    print(f"No, maxdiff={max_diff}")
