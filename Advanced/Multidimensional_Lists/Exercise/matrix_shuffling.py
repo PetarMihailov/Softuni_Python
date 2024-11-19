028769c5c00db022f7ea88463ebc9d1bdd54fc7f
pmihailov <petar_mihailov@gbg.bg>
2024-11-19 11:24:44 +0200

    Update matrix_shuffling.py

--- Advanced/Multidimensional_Lists/Exercise/matrix_shuffling.py
+++ Advanced/Multidimensional_Lists/Exercise/matrix_shuffling.py
@@ -34,3 +34,39 @@ while True:
         print_matrix(matrix)
     else:
         print("Invalid input!")
+
+# second solution
+
+def print_matrix(matr):
+    for row in matr:
+        print(" ".join(map(str, row)))
+
+def is_valid_command(data, rows, cols):
+    if not data[0] == "swap" or not len(data) == 5:
+        return False
+    try:
+        row_1, col_1, row_2, col_2 = map(int, data[1:])
+    except ValueError:
+        return False
+
+    if row_1 < 0 or row_1 >= rows or col_1 < 0 or col_1 >= cols or \
+        row_2 < 0 or row_2 >= rows or col_2 < 0 or col_2 >= cols:
+        return False
+    return True
+
+rows, cols = map(int, input().split())
+
+matrix = [input().split() for _ in range(rows)]
+
+command = input().split()
+
+while not command[0] == "END":
+
+    if is_valid_command(command, rows, cols):
+        row_1, col_1, row_2, col_2 = map(int, command[1:])
+        matrix[row_1][col_1], matrix[row_2][col_2] = matrix[row_2][col_2], matrix[row_1][col_1]
+        print_matrix(matrix)
+    else:
+        print("Invalid input!")
+
+    command = input().split()
