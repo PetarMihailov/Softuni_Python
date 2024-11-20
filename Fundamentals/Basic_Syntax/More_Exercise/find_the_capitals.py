3f5e5c3777518879e1bb8e3d25f243e5ced656fd
pmihailov <petar_mihailov@gbg.bg>
2024-11-20 11:13:21 +0200

    Update find_the_capitals.py
    solution with enumerate

--- Fundamentals/Basic_Syntax/More_Exercise/find_the_capitals.py
+++ Fundamentals/Basic_Syntax/More_Exercise/find_the_capitals.py
@@ -3,3 +3,11 @@ word = list(input())
 capitals = [index for index in range(len(word)) if word[index].isupper()]
 
 print(capitals)
+
+# second solution
+
+string = input()
+
+indexes = [index for index, char in enumerate(string) if char.isupper()]
+
+print(indexes)
