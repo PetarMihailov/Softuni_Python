ba04923f00fdb2f3f86ac0da15812dd17a8e9eeb
pmihailov <petar_mihailov@gbg.bg>
2024-11-21 10:55:48 +0200

    Update josephus_permutation.py

--- Fundamentals/Lists_Basics/More_Exercise/josephus_permutation.py
+++ Fundamentals/Lists_Basics/More_Exercise/josephus_permutation.py
@@ -11,3 +11,17 @@ while numbers:
     result.append(executed)
 
 print(f"[{','.join(map(str, result))}]")
+
+#  second solution
+
+people = list(map(int, input().split()))
+count = int(input())
+
+result = []
+index = 0
+
+while people:
+    index = (index + count - 1) % len(people)
+    result.append(people.pop(index))
+
+print(f"[{','.join(map(str, result))}]")
