5dfeec12287c8c41a37a6da915fcc5ffba080731
pmihailov <petar_mihailov@gbg.bg>
2025-01-03 14:33:37 +0200

    Update list_statistics.py

--- Fundamentals/Lists_Basics/Lab/list_statistics.py
+++ Fundamentals/Lists_Basics/Lab/list_statistics.py
@@ -9,3 +9,14 @@ print(positives)
 print(negatives)
 print(f"Count of positives: {len(positives)}")
 print(f"Sum of negatives: {sum(negatives)}")
+
+# 
+
+nums = [int(input()) for _ in range(int(input()))]
+
+positives = [num for num in nums if num >= 0]
+negatives = [num for num in nums if num < 0]
+
+print(f"{positives}\n{negatives}\nCount of positives: {len(positives)}\nSum of negatives: {sum(negatives)}")
+
+
