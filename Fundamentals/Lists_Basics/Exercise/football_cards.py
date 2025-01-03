2538899dbb8a66af2e849b8675cf1cc89449ef10
pmihailov <petar_mihailov@gbg.bg>
2025-01-03 15:31:48 +0200

    Update football_cards.py

--- Fundamentals/Lists_Basics/Exercise/football_cards.py
+++ Fundamentals/Lists_Basics/Exercise/football_cards.py
@@ -1,5 +1,5 @@
-team_a = [int(el) for el in range(1, 12)]
-team_b = [int(el) for el in range(1, 12)]
+team_a = [el for el in range(1, 12)]
+team_b = [el for el in range(1, 12)]
 
 cards = input().split()
 flag =  False
@@ -20,3 +20,29 @@ if not flag:
     print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
 else:
     print(f"Team A - {len(team_a)}; Team B - {len(team_b)}\nGame was terminated")
+
+# 
+
+team_a = [str(num) for num in range(1, 12)]
+team_b = [str(num) for num in range(1, 12)]
+
+cards = input().split()
+
+is_terminated = False
+
+for card in cards:
+    team, player = card.split("-")
+
+    if team == "A" and player in team_a:
+        team_a.remove(player)
+    elif team == "B" and player in team_b:
+        team_b.remove(player)
+
+    if len(team_a) < 7 or len(team_b) < 7:
+        is_terminated = True
+        break
+
+print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
+
+if is_terminated:
+    print("Game was terminated")
