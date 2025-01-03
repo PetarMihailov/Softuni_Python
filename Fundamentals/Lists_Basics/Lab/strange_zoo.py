1d7f031b1aba5307335ddd3a57182d2a5a4d7e26
pmihailov <petar_mihailov@gbg.bg>
2025-01-03 14:17:01 +0200

    Update strange_zoo.py

--- Fundamentals/Lists_Basics/Lab/strange_zoo.py
+++ Fundamentals/Lists_Basics/Lab/strange_zoo.py
@@ -7,3 +7,11 @@ meerkat = [tail, body, head]
 meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
 
 print(meerkat)
+
+# 
+
+zoo = [input(), input(), input()]
+
+zoo[0], zoo[2] = zoo[2], zoo[0]
+
+print(zoo)
