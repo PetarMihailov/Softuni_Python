e5992038e3ee704e497dc6ea67c4f625379bc552
pmihailov <petar_mihailov@gbg.bg>
2025-01-24 11:14:51 +0200

    Update survival_of_the_biggest.py

--- Fundamentals/Lists_Basics/Exercise/survival_of_the_biggest.py
+++ Fundamentals/Lists_Basics/Exercise/survival_of_the_biggest.py
@@ -5,3 +5,12 @@ for _ in range(n):
     numbers.remove(min(numbers))
 
 print(", ".join([str(el) for el in numbers]))
+
+# second solution
+
+numbers = list(map(int, input().split()))
+count = int(input())
+
+[numbers.remove(min(numbers)) for _ in range(count)]
+
+print(", ".join([str(el) for el in numbers]))
