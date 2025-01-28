42ddc312ae8ec4ba7d53a598bdbabec3d2d3cf4a
pmihailov <petar_mihailov@gbg.bg>
2025-01-28 14:25:24 +0200

    Update bread_factory.py

--- Fundamentals/Lists_Basics/Exercise/bread_factory.py
+++ Fundamentals/Lists_Basics/Exercise/bread_factory.py
@@ -34,3 +34,41 @@ for el in events:
 
 if not is_closed:
     print(f"Day completed!\nCoins: {coins}\nEnergy: {energy}")
+
+# second solution 
+
+data = input().split("|")
+energy = 100
+coins = 100
+
+for el in data:
+    event, points = el.split("-")
+    points = int(points)
+
+    if event == "rest":
+        gained_energy = points
+        if energy + points > 100:
+            gained_energy = 100 - energy
+
+        energy += gained_energy
+        print(f"You gained {gained_energy} energy.\nCurrent energy: {energy}.")
+    elif event == "order":
+        if energy >= 30:
+            energy -= 30
+            coins += points
+            print(f"You earned {points} coins.")
+        else:
+            energy += 50
+            print("You had to rest!")
+    else:
+        if coins >= points:
+            coins -= points
+            print(f"You bought {event}.")
+        else:
+            print(f"Closed! Cannot afford {event}.")
+            break
+
+else:
+    print(f"Day completed!\nCoins: {coins}\nEnergy: {energy}")
+
+
