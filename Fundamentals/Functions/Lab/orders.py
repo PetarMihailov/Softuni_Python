f2f10f6eb4f0a75942f088897e7887e2598ebd96
pmihailov <petar_mihailov@gbg.bg>
2025-01-28 15:52:36 +0200

    Update orders.py

--- Fundamentals/Functions/Lab/orders.py
+++ Fundamentals/Functions/Lab/orders.py
@@ -14,3 +14,23 @@ prod = input()
 quant = float(input())
 
 print(orders(prod, quant))
+
+# second solution
+
+def calculate_order(items, item, quantity):
+    if item in items:
+        return items.get(item) * quantity
+
+
+product = input()
+qty = int(input())
+
+items_qty = {
+    "coffee": 1.50,
+    "coke": 1.40,
+    "water": 1.00,
+    "snacks": 2.00
+}
+
+
+print(f"{calculate_order(items_qty, product, qty):.2f}")
