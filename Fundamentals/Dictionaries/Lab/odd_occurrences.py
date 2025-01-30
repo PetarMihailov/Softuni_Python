9ccdd4aa291591dd9ba3181cc703a0db90afa1cd
pmihailov <petar_mihailov@gbg.bg>
2025-01-30 13:36:37 +0200

    Update odd_occurrences.py

--- Fundamentals/Dictionaries/Lab/odd_occurrences.py
+++ Fundamentals/Dictionaries/Lab/odd_occurrences.py
@@ -5,3 +5,11 @@ occurrences = {key: data.count(key) for key in data}
 for key in occurrences:
     if not occurrences[key] % 2 == 0:
         print(key, end=" ")
+
+# second solution
+
+data = [el.lower() for el in input().split()]
+
+occurrences = {key: data.count(key) for key in data if not data.count(key) % 2 == 0}
+
+print(*occurrences)
