8e525fd8f1f0287dad69e8306b23dd077cc5a69a
pmihailov <petar_mihailov@gbg.bg>
2025-01-30 10:38:14 +0200

    Update factorial_division.py

--- Fundamentals/Functions/Exercise/factorial_division.py
+++ Fundamentals/Functions/Exercise/factorial_division.py
@@ -12,3 +12,20 @@ def division(num_1, num_2):
 first = int(input())
 second = int(input())
 print(f"{division(first, second):.2f}")
+
+# second solution
+
+import math
+
+
+def factorial_division(num1, num2):
+    factorial1 = math.factorial(num1)
+    factorial2 = math.factorial(num2)
+    result = factorial1 / factorial2
+    return f"{result:.2f}"
+
+
+num_1 = int(input())
+num_2 = int(input())
+
+print(factorial_division(num_1, num_2))
