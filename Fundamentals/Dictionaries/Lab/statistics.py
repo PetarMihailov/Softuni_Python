67206878852d96b3f23cdfef4dcd6ae41e741826
pmihailov <petar_mihailov@gbg.bg>
2025-01-30 11:32:59 +0200

    Update statistics.py

--- Fundamentals/Dictionaries/Lab/statistics.py
+++ Fundamentals/Dictionaries/Lab/statistics.py
@@ -18,3 +18,26 @@ for key, val in products.items():
     print(f"- {key}: {val}")
 
 print(f"Total Products: {len(products)}\nTotal Quantity: {sum([qty for qty in products.values()])}")
+
+# second solution
+
+data = input()
+
+products = {}
+
+while not data == 'statistics':
+    key, val = data.split(": ")
+    val = int(val)
+
+    if not key in products:
+        products[key] = 0
+    products[key] += val
+
+    data = input()
+
+print("Products in stock:")
+
+for key, val in products.items():
+    print(f"- {key}: {val}")
+
+print(f"Total Products: {len(products)}\nTotal Quantity: {sum(products.values())}")
