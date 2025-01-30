6e08a1509720b58db7d76681cdeaa389f4dea2e5
pmihailov <petar_mihailov@gbg.bg>
2025-01-30 11:16:52 +0200

    Update backery.py

--- Fundamentals/Dictionaries/Lab/backery.py
+++ Fundamentals/Dictionaries/Lab/backery.py
@@ -8,3 +8,11 @@ for index in range(0, len(products), 2):
     bakery[key] = value
 
 print(bakery)
+
+#  second solution
+
+data = input().split()
+
+products = {data[index]: int(data[index+1]) for index in range(0, len(data), 2)}
+
+print(products)
